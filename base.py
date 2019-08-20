0551d4f2da9a57e2872928ed393435b1da600a88
sugaok <katsuhiko.sugaya@outlook.jp>
2019-08-21 00:25:13 +0900

    Xceptionを手動で実装

--- base.py
+++ base.py
@@ -14,6 +14,53 @@ print('Keras Version: ', keras.__version__)
 # tf.debugging.set_log_device_placement(True)
 
 
+class XceptionModified(keras.Model):
+    def __init__(self):
+        super(XceptionModified, self).__init__()
+        self.bn = keras.layers.normalization.BatchNormalization()
+        self.relu = keras.layers.ReLU()
+        self.mp3_2 = keras.layers.MaxPool2D(3, 2)
+        self.gap = keras.layers.GlobalAveragePooling2D()
+        self.flatten = keras.layers.Flatten()
+
+        self.entry_conv1 = keras.layers.Conv2D(32, 3, 2, activation='relu')
+        self.entry_conv2 = keras.layers.Conv2D(64, 3, 2, activation='relu')
+
+        self.entry_sepconv1 = keras.layers.SeparableConv2D(128, 3, activation='relu')
+        self.entry_sepconv2 = keras.layers.SeparableConv2D(128, 3)
+
+        self.entry_conv3 = keras.layers.Conv2D(128, 1, 2)
+
+        self.entry_sepconv3 = keras.layers.SeparableConv2D(256, 3)
+        self.entry_sepconv4 = keras.layers.SeparableConv2D(256, 3)
+
+        self.entry_conv4 = keras.layers.Conv2D(256, 1, 2)
+
+        self.entry_sepconv5 = keras.layers.SeparableConv2D(728, 3)
+        self.entry_sepconv6 = keras.layers.SeparableConv2D(728, 3)
+
+        self.entry_conv5 = keras.layers.Conv2D(728, 1, 2)
+
+        self.middle_flow = []
+
+        for i in range(8):
+            middle_flow.append([
+                keras.layers.SeparableConv2D(728, 3, name='middle_sepconv1_%d' % (i + 1)),
+                keras.layers.SeparableConv2D(728, 3, name='middle_sepconv2_%d' % (i + 1)),
+                keras.layers.SeparableConv2D(728, 3, name='middle_sepconv3_%d' % (i + 1)),
+            ])
+
+        self.exit_sepconv1 = keras.layers.SeparableConv2D(728, 3)
+        self.exit_sepconv2 = keras.layers.SeparableConv2D(1024, 3)
+
+        self.exit_conv1 = keras.layers.Conv2D(1024, 1, 2)
+
+        self.exit_sepconv3 = keras.layers.SeparableConv2D(1536, 3)
+        self.exit_sepconv4 = keras.layers.SeparableConv2D(2048, 3)
+
+        self.fc = keras.layers.Dense(10, activation='softmax')
+
+
 def main():
     dataset_name = 'cifar10'
     load_kwargs = {
@@ -29,6 +76,7 @@ def main():
         'as_dataset_kwargs': None
     }
     data, info = tfds.load(dataset_name, **load_kwargs)
+    width, height, channels = info.features['image'].shape
 
     def cast_image(image, label):
         image = tf.cast(image, tf.float32)
@@ -38,18 +86,123 @@ def main():
     for key in data:
         data[key] = data[key].map(cast_image, num_parallel_calls=16)
 
-    model = keras.applications.vgg16.VGG16(
-        include_top=True,
-        weights=None,
-        input_tensor=None,
-        input_shape=[32, 32, 3],
-        pooling=None,
-        classes=info.features['label'].num_classes
-    )
+    
+    #####################
+
+    inputs = keras.layers.Input(shape=info.features['image'].shape)
+    x = inputs
+
+    x = keras.layers.Conv2D(32, 3, 2)(x)
+    x = keras.layers.normalization.BatchNormalization()(x)
+    x = keras.layers.ReLU()(x)
+
+    x = keras.layers.Conv2D(64, 3, 2)(x)
+    x = keras.layers.normalization.BatchNormalization()(x)
+    x = keras.layers.ReLU()(x)
+
+    res = keras.layers.Conv2D(128, 1, 2)(x)
+    res = keras.layers.normalization.BatchNormalization()(res)
+
+    x = keras.layers.SeparableConv2D(128, 3)(x)
+    x = keras.layers.normalization.BatchNormalization()(x)
+
+    x = keras.layers.ReLU()(x)
+    x = keras.layers.SeparableConv2D(128, 3)(x)
+    x = keras.layers.normalization.BatchNormalization()(x)
+
+    x = keras.layers.MaxPool2D(3, 2)(x)
+
+    x = keras.layers.Add()([x, res])
+
+    res = keras.layers.Conv2D(256, 1, 2)(x)
+    res = keras.layers.normalization.BatchNormalization()(res)
+
+    x = keras.layers.ReLU()(x)
+    x = keras.layers.SeparableConv2D(256, 3)(x)
+    x = keras.layers.normalization.BatchNormalization()(x)
+
+    x = keras.layers.ReLU()(x)
+    x = keras.layers.SeparableConv2D(256, 3)(x)
+    x = keras.layers.normalization.BatchNormalization()(x)
+
+    x = keras.layers.MaxPool2D(3, 2)(x)
+
+    x = keras.layers.Add()([x, res])
+
+    res = keras.layers.Conv2D(728, 1, 2)(x)
+    res = keras.layers.normalization.BatchNormalization()(res)
+
+    x = keras.layers.ReLU()(x)
+    x = keras.layers.SeparableConv2D(728, 3)(x)
+    x = keras.layers.normalization.BatchNormalization()(x)
+
+    x = keras.layers.ReLU()(x)
+    x = keras.layers.SeparableConv2D(728, 3)(x)
+    x = keras.layers.normalization.BatchNormalization()(x)
+
+    x = keras.layers.MaxPool2D(3, 2)(x)
+
+    x = keras.layers.Add()([x, res])
+
+    for i in range(8):
+        res = x
+        x = keras.layers.SeparableConv2D(728, 3, name='middle_sepconv1_%d' % (i + 1))(x)
+        x = keras.layers.normalization.BatchNormalization()(x)
+        x = keras.layers.SeparableConv2D(728, 3, name='middle_sepconv2_%d' % (i + 1))(x)
+        x = keras.layers.normalization.BatchNormalization()(x)
+        x = keras.layers.SeparableConv2D(728, 3, name='middle_sepconv3_%d' % (i + 1))(x)
+        x = keras.layers.normalization.BatchNormalization()(x)
+        x = keras.layers.Add()([x, res])
+
+    res = keras.layers.Conv2D(1024, 1, 2)(x)
+    res = keras.layers.normalization.BatchNormalization()(res)
+
+    x = keras.layers.ReLU()(x)
+    x = keras.layers.SeparableConv2D(728, 3)(x)
+    x = keras.layers.normalization.BatchNormalization()(x)
+
+    x = keras.layers.ReLU()(x)
+    x = keras.layers.SeparableConv2D(1024, 3)(x)
+    x = keras.layers.normalization.BatchNormalization()(x)
+
+    x = keras.layers.MaxPool2D(3, 2)(x)
+
+    x = keras.layers.Add()([x, res])
+
+    x = keras.layers.SeparableConv2D(1536, 3)(x)
+    x = keras.layers.normalization.BatchNormalization()(x)
+    x = keras.layers.ReLU()(x)
+
+    x = keras.layers.SeparableConv2D(2048, 3)(x)
+    x = keras.layers.normalization.BatchNormalization()(x)
+    x = keras.layers.ReLU()(x)
+
+    x = keras.layers.GlobalAveragePooling2D()(x)
+
+    x = keras.layers.Flatten()(x)
+    outputs = keras.layers.Dense(info.features['label'].num_classes, activation='softmax')(x)
+
+    # model = keras.applications.vgg16.VGG16(
+    #     include_top=True,
+    #     weights=None,
+    #     input_tensor=None,
+    #     input_shape=[32, 32, 3],
+    #     pooling=None,
+    #     classes=info.features['label'].num_classes
+    # )
+    # model = keras.applications.xception.Xception(
+    #     include_top=True,
+    #     weights=None,
+    #     input_tensor=None,
+    #     input_shape=info.features['image'].shape,
+    #     pooling=None,
+    #     classes=info.features['label'].num_classes
+    # )
+    model = keras.Model(inputs=inputs, outputs=outputs)
     model.summary()
 
     loss_object = keras.losses.SparseCategoricalCrossentropy()
-    optimizer = keras.optimizers.Adam()
+    optimizer = keras.optimizers.Nadam()
 
     train_loss = keras.metrics.Mean(name='train_loss')
     train_accuracy = keras.metrics.SparseCategoricalAccuracy(name='train_accuracy')
@@ -73,9 +226,9 @@ def main():
     @tf.function
     def test_step(image, label):
         predictions = model(image)
-        t_loss = loss_object(label, predictions)
+        loss = loss_object(label, predictions)
 
-        test_loss(t_loss)
+        test_loss(loss)
         test_accuracy(label, predictions)
 
     file_result = open('result.csv', mode='a')
